82c1eed7857f97b3762966a44790cd1b79615529
irina.sklyarova <irina060981@yandex.ru>
2017-10-03 19:21:34 +1000

    first task is complete

--- js/header.js
+++ js/header.js
@@ -5,4 +5,18 @@
 $(document).ready(function(){
 	"use strict";
 
+	var $modal = $("[rel='js-modal']");
+
+	$("[rel='js-controls']").on("click", "[rel*='js-']", function(evt){
+		evt.preventDefault();
+		evt.stopPropagation();
+		evt.stopImmediatePropagation();
+
+		var url = $(evt.target).attr("href");
+
+		$.ajax(url, { dataType: "text" })
+		.then(function(contents){
+			$modal.html(contents).show();
+		});
+	});
 });
